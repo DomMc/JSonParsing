5497605944c02e030ed577cd01c514a62d18481b
DomMcC <dom.mccollum@outlook.com>
2022-03-13 18:38:48 +0000

    Added comments.

--- main.cpp
+++ main.cpp
@@ -296,8 +296,10 @@ private:
 
 int main()
 {
+	// Construct from JSON file.
 	JSonContainer j("JSON/objects.json");
 
+	// Print all objects and their pairs.
 	for(auto& obj : j.GetObjects())
 	{
 		for(auto& a : obj.GetPairs())
@@ -307,27 +309,44 @@ int main()
 
 		std::cout << std::endl;
 	}
+	std::cout << std::endl;
 
+	// Print the total object and pair counts.
 	int a = j.GetObjectCount();
 	int b = j.GetPairCount();
-	std::cout << "Object Count: " << a << "\t" << "Pair Count: " << b << std::endl << std::endl;
+	std::cout << "Object Count: " << a << "\t" << "Pair Count: " << b << std::endl;
+	std::cout << std::endl;
 
+	// Print pair count per object.
+	for (auto& obj : j.GetObjects())
+	{
+		std::cout << obj.GetPairCount() << std::endl;
+	}
+	std::cout << std::endl;
+
+	// Retrieve and print a key : value pair.
 	std::pair<std::string, std::string> pair = j.GetKeyValuePair(0,2);
 	std::cout << "Key: " << pair.first << "\t" << "Value: " << pair.second << std::endl;
+	std::cout << std::endl;
 
+	// Retrieve and print a value using object index and key name.
 	std::cout << j[0]["first"] << std::endl;
 	std::cout << j[1]["new key"] << std::endl;
+	std::cout << std::endl;
 
+	// Retrieve and print a value using key name.
 	std::cout << j("third") << std::endl;
 	std::cout << j("new key 2") << std::endl;
-
-	JsonObject obj = j.GetObject("Object 1");
-
 	std::cout << std::endl;
+
+	// Retrieve and print a value using object name and key name.
+	JsonObject obj = j.GetObject("Object 1");
 	std::cout << j["Object 1"]["first"] << std::endl;
 	std::cout << j["Object 2"]["seventh"] << std::endl;
+	std::cout << std::endl;
 
-	std::cout << std::endl << obj.GetName() << std::endl;
+	// Rerieve an object's name, then print it and all of its key : value pairs.
+	std::cout << obj.GetName() << std::endl;
 	for(auto& p : obj.GetPairs())
 	{
 		std::cout << p.First() << " : " << p.Second() << std::endl;
