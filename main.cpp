7fd4c2bc45b1c84c60eae9a1bd81e7508aa3aa2c
Dominic McCollum <98114338+DomMc@users.noreply.github.com>
2024-01-20 17:03:42 +0000

    Basic parser
    Basic text parser which works with .sav files

--- main.cpp
+++ main.cpp
@@ -1,75 +1,103 @@
-/*
-	Parse a JSON file into a string and then process the string to retrieve the information
-	within the file.
-*/
+
 #include <iostream>
+#include <string>
+#include <utility>
+#include <fstream>
 
-#include "JsonContainer.h"
-#include "JsonObject.h"
-#include "JsonPair.h"
-
-typedef std::vector<JsonObject>::iterator::value_type ObjectIteratorValueType;
-typedef std::vector<JsonPair>::iterator::value_type PairIteratorValueType;
-
-// Test harness.
 int main()
 {
-	// Construct from JSON file.
-	JsonContainer j("JSON/array.json");
+	std::string filename = "C:/Users/dommc/Desktop/SaveFiles/Simi.sav";
+	std::string inString;
 
-	// Print all objects and their pairs.
-	for(ObjectIteratorValueType& obj : j.GetObjects())
+	std::ifstream file(filename);
+	if (file.is_open())
 	{
-		for(PairIteratorValueType& a : obj.GetPairs())
+		inString = std::string(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+	}
+	file.close();
+
+	constexpr char OBJECT_START = ':';
+	constexpr char OBJECT_END = ' ';
+	constexpr char OBJECT_END_ALT = '	';
+	constexpr char LINE_END_MARKER = '#';
+	constexpr char DATA_END_MARKER = '}';
+	constexpr char FILE_END_MARKER = '{';
+
+	constexpr std::string::size_type MARKER = 1;
+	constexpr std::string::size_type SPACE = 1;
+
+	// Position read markers
+	std::string::size_type startRead	= inString.find(OBJECT_START) + MARKER + SPACE;
+	std::string::size_type endRead		= inString.find(OBJECT_END, startRead);
+	std::string::size_type lineEnd		= inString.find(LINE_END_MARKER, startRead);
+	std::string::size_type dataEnd		= inString.find(DATA_END_MARKER, startRead);
+	std::string::size_type fileEnd		= inString.find(FILE_END_MARKER, startRead);
+
+	// Prepare to write data
+	std::string outString = "";
+	std::string delimiter = ",";
+
+	// Example string
+	// #Action: Highlight Target:BP_Target_C_2147482264	Seconds: 43	Partial: 0.685673	TargetX: 71.11575	TargetY: 36.595354	TargetZ: -68.963895	MouseX: 976.0	MouseY: 322.0	Key Required: S
+	// #Action: Destroy Target:BP_Target_C_2147482264	Seconds:44	Partial: 0.310433	TargetX: 71.11575	TargetY: 36.595354	TargetZ: -68.963895	MouseX: 1009.0	MouseY: 307.0	Deviation Squared: 952745.5625	Key Required: S	Key Pressed: S
+
+	while (dataEnd < fileEnd)
+	{
+		outString.append("NEW SESSION,");
+
+		while (startRead < dataEnd)
 		{
-			std::cout << a.First() << " : " << a.Second() << std::endl;
+			uint32_t variableCount = 1;
+
+			while (startRead < lineEnd)
+			{
+				// Write result into output string.
+				outString.append(inString.substr(startRead, endRead - startRead));
+				outString.append(delimiter);
+				
+				variableCount++;
+				
+				// Jump to the start of the next variable.
+				startRead = inString.find(OBJECT_START, startRead);
+				startRead += variableCount > 2 ? MARKER + SPACE : MARKER;
+
+				// Reposition the end marker
+				std::string::size_type end		= inString.find(OBJECT_END, startRead);
+				std::string::size_type endAlt	= inString.find(OBJECT_END_ALT, startRead);
+				
+				// Pick the nearest end of variable delimiter. Both space and tab are valid! :-(
+				endRead = end < endAlt ? end : endAlt;
+
+				// Special case for the end of a line
+				if (endRead > lineEnd)
+				{
+					endRead = startRead + SPACE;
+				}
+			}
+
+			// Find the next line delimiter
+			lineEnd = inString.find(LINE_END_MARKER, startRead);
+			outString.append("\n");
+
+			std::cout << outString;
+			outString.clear();
 		}
 
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
+		// Find the next data delimiter
+		dataEnd = inString.find(DATA_END_MARKER, startRead);
 
-	// Print the total object and pair counts.
-	int a = j.GetObjectCount();
-	int b = j.GetPairCount();
-	std::cout << "Object Count: " << a << "\t" << "Pair Count: " << b << std::endl;
-	std::cout << std::endl;
+		// Find the next line delimiter
+		lineEnd = inString.find(LINE_END_MARKER, startRead);
 
-	// Print pair count per object.
-	for (auto& obj : j.GetObjects())
-	{
-		std::cout << obj.GetPairCount() << std::endl;
-	}
-	std::cout << std::endl;
+		// Find the next variable
+		startRead = inString.find(OBJECT_START, lineEnd);
+		startRead += MARKER;
 
-	// Retrieve and print a key : value pair.
-	std::pair<std::string, std::string> pair = j.GetKeyValuePair(0,2);
-	std::cout << "Key: " << pair.first << "\t" << "Value: " << pair.second << std::endl;
-	std::cout << std::endl;
-
-	// Retrieve and print a value using object index and key name. Print an error message if not found.
-	std::cout << j[0]["first"] << std::endl;
-	std::cout << j[1]["new key"] << std::endl;
-	std::cout << std::endl;
-
-	// Retrieve and print a value using key name. Print an error message if not found.
-	std::cout << j("third") << std::endl;
-	std::cout << j("new key 2") << std::endl;
-	std::cout << std::endl;
-
-	// Retrieve and print a value using object name and key name.
-	JsonObject obj = j.GetObject("Object 1");
-	std::cout << j["Object 1"]["first"] << std::endl;
-	std::cout << j["Object 2"]["seventh"] << std::endl;
-	std::cout << std::endl;
-
-	// Rerieve an object's name, then print it and all of its key : value pairs.
-	std::cout << obj.GetName() << std::endl;
-	for(auto& p : obj.GetPairs())
-	{
-		std::cout << p.First() << " : " << p.Second() << std::endl;
+		// Reposition the end of entry marker
+		endRead = inString.find(OBJECT_END, startRead);
 	}
 	
+
 	return 0;
 }
 
